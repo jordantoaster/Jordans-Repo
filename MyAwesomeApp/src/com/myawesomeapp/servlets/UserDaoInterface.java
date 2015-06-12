5dfc4924de93242471c75d228453da4faa56df33
jordantoaster <jordanmcdonaldmain@gmail.com>
2015-06-12 16:47:10 +0100

    removal

--- MyAwesomeApp/src/com/myawesomeapp/servlets/UserDaoInterface.java
+++
@@ -1,7 +0,0 @@
-package com.myawesomeapp.servlets;
-
-/*Defines methods required by Dao*/
-public interface UserDaoInterface {
-	boolean insertUser(User user);
-	boolean readAndCompare(User user);
-}
