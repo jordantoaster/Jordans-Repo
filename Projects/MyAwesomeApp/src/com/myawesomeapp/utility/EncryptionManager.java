0cb54b86ee693b97c2df52e705133d9c5815e042
jordantoaster <jordanmcdonaldmain@gmail.com>
2015-07-13 17:37:21 +0100

    refactor of DAO code and servlets related to login to allow for encryption using a new library which uses salt and hashes

---
+++ Projects/MyAwesomeApp/src/com/myawesomeapp/utility/EncryptionManager.java
@@ -0,0 +1,28 @@
+package com.myawesomeapp.utility;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+/*
+ * In this program I use jaspyt only for instances that require an encrypted password to be evaluated -
+ * during login and during the encrpytion process
+ * otherwise the plain password is used for comparison with the build in library functions
+ * refer to login servlet to see when both are used in tandem
+ * possible refactor to make that code clearer
+ * */
+public class EncryptionManager {
+	
+    StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+	
+	//Library stores the salt inside the has itself, salt is random and changes hash each time
+	public String encrypt(String plainText){
+		
+		String encryptedPassword = passwordEncryptor.encryptPassword(plainText);
+  
+		return encryptedPassword;
+	}
+	
+	//checks the user has entered a correct password
+	public boolean checkPassword(String submittedPassword, String storedPassword){	
+	    return passwordEncryptor.checkPassword(submittedPassword, storedPassword);	
+	}
+	
+}
